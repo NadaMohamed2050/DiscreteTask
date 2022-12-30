d6fdc41179a7008cbb75ea74a1c6574cdc8bcf2d
NadaMohamed2050 <117907136+NadaMohamed2050@users.noreply.github.com>
2022-12-30 23:15:34 +0200

    Add files via upload

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace primenumber
+{
+    internal class programe
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the frist number");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the second number");
+            int n2 = int.Parse(Console.ReadLine());
+
+            //read the number
+
+            Console.Write("the prime number is:");
+
+            for(int p =n1; p<n2 ;p++)
+            {
+                int num=0;
+                for (num = 2; num < p; num++)
+                {
+                    if (p % num ==0)
+                                      break;
+                }
+                    if (num == p)
+                   
+                        Console.Write( + p+",");
+            }
+        }
+    }
+}
